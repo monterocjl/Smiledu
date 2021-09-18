3e788ab9dde745122763213b66ecbcd24e6319cf
Juan Montero <jmonteroc.91@gmail.com>
2021-09-18 14:48:47 -0500

    Update

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// Para ocultar y mostrar la Sidebar
+let sidebarBtn = document.querySelector(".smiledu-sidebar_header_menu-btn");
+let sidebar = document.querySelector(".smiledu-sidebar");
+    sidebarBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("close");
+    });
+
+
+// Para desplegar el detalle de las herramientas del sidebar panel
 let matriculaBtn = document.querySelector(".btn-matricula");
 let subMenu = document.querySelector(".sub-menu");
 let arrow = document.querySelector(".arrow-selected");
@@ -7,19 +16,14 @@ let arrow = document.querySelector(".arrow-selected");
     matriculaBtn.classList.toggle("showMenu");
     });
 
-let sidebarBtn = document.querySelector(".smiledu-sidebar_header_menu-btn");
-let sidebar = document.querySelector(".smiledu-sidebar");
-    sidebarBtn.addEventListener("click", ()=>{
-        sidebar.classList.toggle("close");
-    });
 
-
+// Para desplegar el detalle de las Card de los grados del colegio elegido
 const cardCollageBtn = document.querySelectorAll(".btn-card-collage");
 for (let i = 0; i < cardCollageBtn.length; i++) {
     cardCollageBtn[i].addEventListener("click", function () {
         const card = this.parentElement;
         const cardCollageSubMenu = this.nextElementSibling;
-        
+
         cardCollageSubMenu.classList.toggle("showDetails");
         card.classList.toggle("showDetails");
 })}
