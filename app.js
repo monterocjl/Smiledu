d113c0643cb472993c60c4dcb293bbd42cf44312
Juan Montero <jmonteroc.91@gmail.com>
2021-09-18 14:38:37 -0500

    Update
    Cards collage levels

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let matriculaBtn = document.querySelector(".btn-matricula");
 let subMenu = document.querySelector(".sub-menu");
 let arrow = document.querySelector(".arrow-selected");
-
-
     matriculaBtn.addEventListener("click", ()=>{
     subMenu.classList.toggle("showMenu");
     arrow.classList.toggle("showMenu");
@@ -11,8 +9,17 @@ let arrow = document.querySelector(".arrow-selected");
 
 let sidebarBtn = document.querySelector(".smiledu-sidebar_header_menu-btn");
 let sidebar = document.querySelector(".smiledu-sidebar");
-
-
     sidebarBtn.addEventListener("click", ()=>{
         sidebar.classList.toggle("close");
     });
+
+
+const cardCollageBtn = document.querySelectorAll(".btn-card-collage");
+for (let i = 0; i < cardCollageBtn.length; i++) {
+    cardCollageBtn[i].addEventListener("click", function () {
+        const card = this.parentElement;
+        const cardCollageSubMenu = this.nextElementSibling;
+        
+        cardCollageSubMenu.classList.toggle("showDetails");
+        card.classList.toggle("showDetails");
+})}
